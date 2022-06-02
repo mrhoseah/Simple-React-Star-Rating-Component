7420df43be6ff47807a44fdf41e7521d4b19c5a6
mrhoseah <hoseahkplgt@gmail.com>
2022-06-02 13:21:22 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+import StarRating from './components/StarRating'
+
+function App() {
+
+  return (
+    <div className="App">
+      <StarRating />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/StarRating.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+function StarRating() {
+  return (
+    <div className='mx-auto py-12 px-6 bg-black'>StarRating</div>
+  )
+}
+
+export default StarRating
